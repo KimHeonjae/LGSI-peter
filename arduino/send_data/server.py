84eb16908cb7295ef05f19d9a4deb97f8bf1d312
Gyu-won <milestogo0905@naver.com>
2023-07-13 19:08:23 +0900

    feat/hardware/add ph sensor code and refactoring arduino code

--- arduino/send_data/server.py
+++ arduino/send_data/server.py
@@ -3,7 +3,7 @@ import serial
 import time
 
 # 스프링 부트 서버의 엔드포인트 URL
-url = "http://192.168.201.4:8080/raspberry/save"
+url = "http://192.168.201.4:8080/api/data/save"
 
 py_serial = serial.Serial(
     port='COM13',
@@ -20,34 +20,33 @@ def get_data_from_sensor():
 
 
 while True:
-    for city in range(2):
-        waterLevel = get_data_from_sensor()
-        temperature = get_data_from_sensor()
-        ph = get_data_from_sensor()
-        turbidity = get_data_from_sensor()
+    cityCode = get_data_from_sensor()
+    waterLevel = get_data_from_sensor()
+    temperature = get_data_from_sensor()
+    ph = get_data_from_sensor()
+    turbidity = get_data_from_sensor()
 
-        if city == 0:
-            cityName = "Ulsan"
-        else:
-            cityName = "Busan"
+    if cityCode == '0':
+        cityName = "Bengaluru"
+    else:
+        cityName = "Delhi"
 
-        # make data
-        data = {
-            "cityName": cityName,
-            "waterLevel": waterLevel,
-            "temperature": temperature,
-            "ph": 2,
-            "turbidity": 3
-        }
-        print(data)
+    # make data
+    data = {
+        "cityName": cityName,
+        "waterLevel": waterLevel,
+        "temperature": temperature,
+        "ph": ph,
+        "turbidity": turbidity
+    }
+    print(data)
 
-        # POST 요청 보내기
-        response = requests.post(url, json=data)
+    # POST 요청 보내기
+    response = requests.post(url, json=data)
 
-        # 응답 확인
-        if response.status_code == 200:
-            print("request success")
-            print("data:", response.json())
-        else:
-            print("error, status code:", response.status_code)
-
+    # 응답 확인
+    if response.status_code == 200:
+        print("request success")
+        print("data:", response.json())
+    else:
+        print("error, status code:", response.status_code)
